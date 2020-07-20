23ced64922f3d0de009d3fac9a8d6eb5a7d677c8
HyunCello <jeremykim95@gmail.com>
2020-07-20 20:57:54 +0900

    edit menubar

--- webOS/sampleApp/javascript/function.js
+++ webOS/sampleApp/javascript/function.js
@@ -1,5 +1,13 @@
 $(function () {
-  $("#auctions").click(function () {
-    $("#menu").animate({ width: "toggle" }, 350);
+  $("#auctions1").click(function () {
+    $("#menu").animate({ marginLeft: "0%" }, 400);
+    $("#menu").show();
+    $("#auctions1").hide();
+    $("#auctions2").show();
+  });
+  $("#auctions2").click(function () {
+    $("#menu").animate({ marginLeft: "-18%" }, 400);
+    $("#auctions2").hide();
+    $("#auctions1").show();
   });
 });
